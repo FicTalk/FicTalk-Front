3d2fd3ee7d5499b10e5c7a6d7c1bab9aecda124e
jwjung5038 <jwjung5038@innerverz.com>
2024-10-17 18:36:26 +0900

    prettier 적용 및 font 파일 src 외부로 이동

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,38 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-import GNB from "@/components/GNB";
-import { ThemeProvider } from "@/components/themeProvider";
-import SideMenu from "@/components/sideMenu";
-import GoogleLoginProvider from "@/components/googleLoginProvier";
-import { Toaster } from "@/components/ui/sonner";
+import type { Metadata } from 'next'
+import localFont from 'next/font/local'
+import './globals.css'
+import GNB from '@/components/GNB'
+import { ThemeProvider } from '@/components/ThemeProvider'
+import SideMenu from '@/components/SideMenu'
+import GoogleLoginProvider from '@/components/GoogleLoginProvier'
+import { Toaster } from '@/components/ui/sonner'
+import { ReactNode } from 'react'
+
 const pretendard = localFont({
-	src: "../assets/fonts/PretendardVariable.woff2",
-	display: "swap",
-	weight: "45 920",
-});
+    src: '../../public/fonts/PretendardVariable.woff2',
+    display: 'swap',
+    weight: '45 920',
+    variable: '--font-pretendard',
+})
 
 export const metadata: Metadata = {
-	title: "TOONS - 웹툰 알람 서비스",
-	description: "웹툰 알람 서비스",
-	keywords: "웹툰, 네이버 웹툰, 카카오 웹툰",
-	icons: {
-		icon: "/favicon.svg",
-	},
-	openGraph: {
-		description: "웹툰 알람 서비스",
-		url: "https://toons.woos.dev",
-		type: "website",
-		siteName: "TOONS",
-	},
-	twitter: {
-		description: "웹툰 알람 서비스",
-		site: "https://toons.woos.dev",
-	},
-};
+    title: 'TOONS - 웹툰 알람 서비스',
+    description: '웹툰 알람 서비스',
+    keywords: '웹툰, 네이버 웹툰, 카카오 웹툰',
+    icons: {
+        icon: '/favicon.svg',
+    },
+    openGraph: {
+        description: '웹툰 알람 서비스',
+        url: 'https://toons.woos.dev',
+        type: 'website',
+        siteName: 'TOONS',
+    },
+    twitter: {
+        description: '웹툰 알람 서비스',
+        site: 'https://toons.woos.dev',
+    },
+}
 
 /**
  * 다크 모드를 켤려면 default = 'system', enableSystem = true로 설정해주세요.
@@ -39,24 +42,29 @@ export const metadata: Metadata = {
  */
 
 export default function RootLayout({
-	children,
+    children,
 }: Readonly<{
-	children: React.ReactNode;
+    children: ReactNode
 }>) {
-	return (
-		<html lang="ko" suppressHydrationWarning>
-			<body className={`bg-side ${pretendard.className} antialiased`}>
-				<GoogleLoginProvider>
-					<ThemeProvider attribute="class" defaultTheme="light" enableSystem={false} disableTransitionOnChange>
-						<div className="relative flex flex-col min-h-screen mx-auto max-w-[600px] min-h-screen bg-background">
-							<SideMenu />
-							{children}
-							<GNB />
-						</div>
-						<Toaster position="top-center" visibleToasts={1} richColors toastOptions={{ duration: 2000 }} />
-					</ThemeProvider>
-				</GoogleLoginProvider>
-			</body>
-		</html>
-	);
+    return (
+        <html lang="ko" suppressHydrationWarning>
+            <body className={`bg-side ${pretendard.variable} antialiased`}>
+                <GoogleLoginProvider>
+                    <ThemeProvider
+                        attribute="class"
+                        defaultTheme="light"
+                        enableSystem={false}
+                        disableTransitionOnChange
+                    >
+                        <div className="relative flex flex-col min-h-screen mx-auto max-w-[600px] min-h-screen bg-background">
+                            <SideMenu />
+                            {children}
+                            <GNB />
+                        </div>
+                        <Toaster position="top-center" visibleToasts={1} richColors toastOptions={{ duration: 2000 }} />
+                    </ThemeProvider>
+                </GoogleLoginProvider>
+            </body>
+        </html>
+    )
 }
